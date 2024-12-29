c60f28fb4e3e93df9e59a2cb8eeb70c5467b4e5d
gokulakannan19 <sgokulakannan19.jc@gmail.com>
2024-12-29 23:32:35 +0530

    table extracted from excel and added in pdf

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ filepaths = glob.glob("invoices/*.xlsx")
 # print(files)
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath)
 
     filename = Path(filepath).stem
     invoice_no = filename.split("-")[0]
@@ -22,13 +21,28 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=0, h=8, txt=invoice_no, ln=1, align="L", border=0)
     pdf.cell(w=0, h=8, txt=date, ln=1, align="L", border=0)
+    pdf.ln()
 
-    # pdf.ln()
+    df = pd.read_excel(filepath)
+
     # Table
-    # pdf.set_font(family="Times", size=18, style="B")
-    # for index, row in df.iterrows()
-    # pdf.cell(w=35, h=12, txt="product_id", align="L", border=1)
-    # pdf.cell(w=35, h=12, txt="product_id", align="L", border=1)
+    columns = df.columns
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], align="L", border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], align="L", border=1)
+    pdf.cell(w=35, h=8, txt=columns[2], align="L", border=1)
+    pdf.cell(w=25, h=8, txt=columns[3], align="L", border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], align="L", border=1, ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), align="L", border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), align="L", border=1)
+        pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]), align="R", border=1)
+        pdf.cell(w=25, h=8, txt=str(row["price_per_unit"]), align="R", border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), align="R", border=1, ln=1)
     pdf.output(f"pdf/{filename}.pdf")
 
 
